8c7c6ff6e08ccb2bab6093be103cd676ddda5046
careah <118859844+careah@users.noreply.github.com>
2023-05-10 22:39:08 -0500

    Add files via upload

---
+++ EmployeeSQL.sql
@@ -0,0 +1,46 @@
+--employee number, last name, first name, sex, and salary of each employee
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
+
+--first name, last name, and hire date for the employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+--manager of each department along with their department number, department name, employee number, last name, and first name
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN departments d ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+--department number for each employee along with that employee’s employee number, last name, first name, and department name
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN departments d ON de.dept_no = d.dept_no
+JOIN employees e ON de.emp_no = e.emp_no;
+
+--first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--each employee in the Sales department, including their employee number, last name, and first name
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+--each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+--frequency counts, in descending order, of all the employee last names
+SELECT last_name, COUNT(*) as frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sql-challenge
+
+There are some composite keys that are not on the code because I'm still unsure how to use PGAdmin after watching many youtube videos and rewatching the class recordings. I also went to tutoring and they were extremely unhelpful so I'm going to list what is missing because I don't know how to fix it.
+
+The dept_manager table has a composite primary key for emp_no
+
+The dept_emp table has two composite primpary keys for both emp_no and dept_no
